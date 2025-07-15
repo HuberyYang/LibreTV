868d570cec8209a25728a2a8d8d1a7ac472007e1
minliang <569738793@qq.com>
2025-07-15 15:10:51 +0800

    适配

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+  ],
+})
